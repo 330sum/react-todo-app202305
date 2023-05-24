73bca78b563ada2c8562d5261d538b2d493cb745
330sum <126743151+330sum@users.noreply.github.com>
2023-05-24 12:04:28 +0900

    # 4. 할 일 목록 랜더링

--- src/component/TodoMain.js
+++ src/component/TodoMain.js
@@ -3,12 +3,35 @@ import TodoItem from './TodoItem'
 
 import './scss/TodoMain.scss';
 
-const TodoMain = () => {
+const TodoMain = ({ todoList }) => {
+// const TodoMain = (ppprops) => {
+
+    // console.log(ppprops.todoList);
+
+    const renderTodoItem = () => {
+
+        // const todoItems = [];
+        // for(const todo of todoList) {
+        //     todoItems.push(<TodoItem />);
+        // }
+
+        // const todoItems = () => {
+        //     todoList.map(todo => <TodoItem />);
+        // }
+
+        // return todoItems;
+
+    };
+
+
+
   return (
     <ul className = 'todo-list'>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {
+            // renderTodoItem()
+            // 객체 하나하나가 todo임. 
+            todoList.map(todo => <TodoItem item={todo} />)
+        }
     </ul>
   )
 }
